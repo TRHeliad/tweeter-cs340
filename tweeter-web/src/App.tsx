11485f5228030e0263e5b44db88f1c3a88626d84
Ben Madsen <bmadsenonpc@gmail.com>
2024-10-16 12:24:00 -0600

    Fix keys for ItemScrollers

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
+	BrowserRouter,
+	Navigate,
+	Route,
+	Routes,
+	useLocation,
 } from "react-router-dom";
 import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
@@ -25,100 +25,107 @@ import UserItem from "./components/userItem/UserItem";
 import { FollowService } from "./model/service/FollowService";
 
 const App = () => {
-  const { currentUser, authToken } = useUserInfo();
+	const { currentUser, authToken } = useUserInfo();
 
-  const isAuthenticated = (): boolean => {
-    return !!currentUser && !!authToken;
-  };
+	const isAuthenticated = (): boolean => {
+		return !!currentUser && !!authToken;
+	};
 
-  return (
-    <div>
-      <Toaster position="top-right" />
-      <BrowserRouter>
-        {isAuthenticated() ? (
-          <AuthenticatedRoutes />
-        ) : (
-          <UnauthenticatedRoutes />
-        )}
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<Toaster position="top-right" />
+			<BrowserRouter>
+				{isAuthenticated() ? (
+					<AuthenticatedRoutes />
+				) : (
+					<UnauthenticatedRoutes />
+				)}
+			</BrowserRouter>
+		</div>
+	);
 };
 
 const AuthenticatedRoutes = () => {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Navigate to="/feed" />} />
-        <Route
-          path="feed"
-          element={
-            <ItemScroller<Status, StatusService>
-              key={1}
-              presenterGenerator={(view: StatusItemView) =>
-                new FeedPresenter(view)
-              }
-              itemComponentGenerator={(item: Status) => (
-                <StatusItem status={item} />
-              )}
-            />
-          }
-        />
-        <Route
-          path="story"
-          element={
-            <ItemScroller<Status, StatusService>
-              key={2}
-              presenterGenerator={(view: StatusItemView) =>
-                new StoryPresenter(view)
-              }
-              itemComponentGenerator={(item: Status) => (
-                <StatusItem status={item} />
-              )}
-            />
-          }
-        />
-        <Route
-          path="followees"
-          element={
-            <ItemScroller<User, FollowService>
-              key={1}
-              presenterGenerator={(view: UserItemView) =>
-                new FolloweePresenter(view)
-              }
-              itemComponentGenerator={(item: User) => <UserItem value={item} />}
-            />
-          }
-        />
-        <Route
-          path="followers"
-          element={
-            <ItemScroller<User, FollowService>
-              key={2}
-              presenterGenerator={(view: UserItemView) =>
-                new FollowerPresenter(view)
-              }
-              itemComponentGenerator={(item: User) => <UserItem value={item} />}
-            />
-          }
-        />
-        <Route path="logout" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/feed" />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<MainLayout />}>
+				<Route index element={<Navigate to="/feed" />} />
+				<Route
+					path="feed"
+					element={
+						<ItemScroller<Status, StatusService>
+							key={"feed"}
+							presenterGenerator={(view: StatusItemView) =>
+								new FeedPresenter(view)
+							}
+							itemComponentGenerator={(item: Status) => (
+								<StatusItem status={item} />
+							)}
+						/>
+					}
+				/>
+				<Route
+					path="story"
+					element={
+						<ItemScroller<Status, StatusService>
+							key={"story"}
+							presenterGenerator={(view: StatusItemView) =>
+								new StoryPresenter(view)
+							}
+							itemComponentGenerator={(item: Status) => (
+								<StatusItem status={item} />
+							)}
+						/>
+					}
+				/>
+				<Route
+					path="followees"
+					element={
+						<ItemScroller<User, FollowService>
+							key={"followees"}
+							presenterGenerator={(view: UserItemView) =>
+								new FolloweePresenter(view)
+							}
+							itemComponentGenerator={(item: User) => (
+								<UserItem value={item} />
+							)}
+						/>
+					}
+				/>
+				<Route
+					path="followers"
+					element={
+						<ItemScroller<User, FollowService>
+							key={"followers"}
+							presenterGenerator={(view: UserItemView) =>
+								new FollowerPresenter(view)
+							}
+							itemComponentGenerator={(item: User) => (
+								<UserItem value={item} />
+							)}
+						/>
+					}
+				/>
+				<Route path="logout" element={<Navigate to="/login" />} />
+				<Route path="*" element={<Navigate to="/feed" />} />
+			</Route>
+		</Routes>
+	);
 };
 
 const UnauthenticatedRoutes = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<Login originalUrl={location.pathname} />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route
+				path="*"
+				element={<Login originalUrl={location.pathname} />}
+			/>
+		</Routes>
+	);
 };
 
 export default App;
