e19c0f5fc79977e9ff364421805425b490906b58
Ben Madsen <bmadsenonpc@gmail.com>
2024-10-21 16:43:47 -0600

    Add PostStatusPresenter tests

--- tweeter-web/src/presenters/PostStatusPresenter.ts
+++ tweeter-web/src/presenters/PostStatusPresenter.ts
@@ -8,11 +8,15 @@ export interface PostStatusView extends MessageView {
 }
 
 export class PostStatusPresenter extends Presenter<PostStatusView> {
-  private statusService: StatusService;
+  private _statusService: StatusService | null = null;
 
   public constructor(view: PostStatusView) {
     super(view);
-    this.statusService = new StatusService();
+  }
+
+  public get statusService() {
+    if (this._statusService == null) this._statusService = new StatusService();
+    return this._statusService;
   }
 
   public async submitPost(
@@ -20,18 +24,24 @@ export class PostStatusPresenter extends Presenter<PostStatusView> {
     currentUser: User,
     authToken: AuthToken
   ) {
-    this.doFailureReportingOperation(async () => {
-      this.view.setIsLoading(true);
-      this.view.displayInfoMessage("Posting status...", 0);
+    this.doFailureReportingOperation(
+      async () => {
+        this.view.setIsLoading(true);
+        this.view.displayInfoMessage("Posting status...", 0);
 
-      const status = new Status(post, currentUser!, Date.now());
+        const status = new Status(post, currentUser!, Date.now());
 
-      await this.statusService.postStatus(authToken, status);
+        await this.statusService.postStatus(authToken, status);
 
-      this.view.setPost("");
-      this.view.displayInfoMessage("Status posted!", 2000);
-    }, "post the status").finally(() => {
-      this.view.clearLastInfoMessage();
+        this.view.clearLastInfoMessage();
+        this.view.setPost("");
+        this.view.displayInfoMessage("Status posted!", 2000);
+      },
+      "post the status",
+      () => {
+        this.view.clearLastInfoMessage();
+      }
+    ).finally(() => {
       this.view.setIsLoading(false);
     });
   }
---
+++ tweeter-web/test/presenters/PostStatusPresenter.test.ts
@@ -0,0 +1,88 @@
+import {
+  anything,
+  capture,
+  instance,
+  mock,
+  spy,
+  verify,
+  when,
+} from "ts-mockito";
+import { StatusService } from "../../src/model/service/StatusService";
+import {
+  PostStatusPresenter,
+  PostStatusView,
+} from "../../src/presenters/PostStatusPresenter";
+import { AuthToken, Status, User } from "tweeter-shared";
+
+describe("PostStatusPresenter", () => {
+  let mockPostStatusView: PostStatusView;
+  let postStatusPresenter: PostStatusPresenter;
+  let mockStatusService: StatusService;
+
+  const authToken = new AuthToken("abc123", Date.now());
+  const user = new User("myname", "isjeff", "jeff", "");
+
+  beforeEach(() => {
+    mockPostStatusView = mock<PostStatusView>();
+    const mockPostStatusViewInstance = instance(mockPostStatusView);
+
+    const postStatusPresenterSpy = spy(
+      new PostStatusPresenter(mockPostStatusViewInstance)
+    );
+    postStatusPresenter = instance(postStatusPresenterSpy);
+
+    mockStatusService = mock<StatusService>();
+    const mockUserServiceInstance = instance(mockStatusService);
+
+    when(postStatusPresenterSpy.statusService).thenReturn(
+      mockUserServiceInstance
+    );
+  });
+
+  it("tells the view to display a posting status message", async () => {
+    await postStatusPresenter.submitPost("Test post", user, authToken);
+    verify(
+      mockPostStatusView.displayInfoMessage("Posting status...", 0)
+    ).once();
+  });
+
+  it("calls postStatus on the post status service with the correct status string and auth token", async () => {
+    const postString = "Test post";
+    await postStatusPresenter.submitPost(postString, user, authToken);
+    const [capturedAuthToken, capturedStatus] = capture(
+      mockStatusService.postStatus
+    ).last();
+    expect(capturedAuthToken).toEqual(authToken);
+    expect(capturedStatus.post).toEqual(postString);
+  });
+
+  it("tells the view to clear the last info message, clear the post, and display a status posted message when posting the status succeeds", async () => {
+    await postStatusPresenter.submitPost("Test post", user, authToken);
+
+    verify(mockPostStatusView.clearLastInfoMessage()).once();
+    verify(mockPostStatusView.setPost("")).once();
+    verify(
+      mockPostStatusView.displayInfoMessage("Status posted!", 2000)
+    ).once();
+  });
+
+  it("displays an error message and clears the last info message and does not tell it to clear the post or display a status posted message when posting the status fails", async () => {
+    const errorMessage = "An error ocurred2";
+    const error = new Error(errorMessage);
+    when(mockStatusService.postStatus(authToken, anything())).thenThrow(error);
+
+    await postStatusPresenter.submitPost("Test post", user, authToken);
+
+    verify(
+      mockPostStatusView.displayErrorMessage(
+        `Failed to post the status because of exception: ${errorMessage}`
+      )
+    ).once();
+
+    verify(mockPostStatusView.clearLastInfoMessage()).once();
+    verify(mockPostStatusView.setPost("")).never();
+    verify(
+      mockPostStatusView.displayInfoMessage("Status posted!", 2000)
+    ).never();
+  });
+});
