42a05af17cceab88550c529fb346127a55b7fa26
Ben Madsen <bmadsenonpc@gmail.com>
2024-11-30 17:39:01 -0700

    Add FollowDto

--- tweeter-shared/src/model/domain/Follow.ts
+++ tweeter-shared/src/model/domain/Follow.ts
@@ -1,27 +1,43 @@
+import { FollowDto } from "../dto/FollowDto";
+import { DtoConvertible } from "./DtoConvertible";
 import { User } from "./User";
 
-export class Follow {
-    private _follower: User;
-    private _followee: User;
+export class Follow extends DtoConvertible<FollowDto> {
+  private _follower: User;
+  private _followee: User;
 
-    public constructor(follower: User, followee: User) {
-        this._follower = follower;
-        this._followee = followee;
-    }
+  public constructor(follower: User, followee: User) {
+    super();
+    this._follower = follower;
+    this._followee = followee;
+  }
 
-    public get follower(): User {
-        return this._follower;
-    }
+  public static fromDto(dto: FollowDto | null): Follow | null {
+    return dto == null
+      ? null
+      : new Follow(User.fromDto(dto.follower)!, User.fromDto(dto.followee)!);
+  }
 
-    public set follower(value: User) {
-        this._follower = value;
-    }
-    
-    public get followee(): User {
-        return this._followee;
-    }
+  public get dto(): FollowDto {
+    return {
+      follower: this.follower.dto,
+      followee: this.followee.dto,
+    };
+  }
 
-    public set followee(value: User) {
-        this._followee = value;
-    }    
+  public get follower(): User {
+    return this._follower;
+  }
+
+  public set follower(value: User) {
+    this._follower = value;
+  }
+
+  public get followee(): User {
+    return this._followee;
+  }
+
+  public set followee(value: User) {
+    this._followee = value;
+  }
 }
