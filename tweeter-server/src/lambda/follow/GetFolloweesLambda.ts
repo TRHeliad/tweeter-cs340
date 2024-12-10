f43a005d67e2699392dd493f932d24c5e6a94ae9
Ben Madsen <bmadsenonpc@gmail.com>
2024-12-10 09:00:57 -0700

    Fix GetFolloweesLambda type

--- tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
+++ tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
@@ -3,7 +3,7 @@ import { UserItemLambda } from "./UserItemLambda";
 import { createFollowService } from "./CreateFollowService";
 
 export const handler = async (
-  request: PagedItemRequest<UserDto>
+  request: PagedItemRequest<string>
 ): Promise<PagedItemResponse<UserDto>> => {
   const followService = createFollowService();
   return UserItemLambda(
